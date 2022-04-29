8c1c9e1b39e2ffdff0b477f36609e81027c07a11
Matheus <matheuspsilva29@gmail.com>
2022-04-29 16:47:27 -0300

    Deletar usuario

--- src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -67,6 +69,15 @@ public class GreetingsController {
     	
     }
     
+    @DeleteMapping(value = "usuario") // Mapeia a URL
+    @ResponseBody //Descrição da resposta
+    public ResponseEntity<String> deletarUsuario(@RequestParam Long id){ // Recebe os dados para salvar
+    	
+    	usuarioRepository.deleteById(id);
+    	
+    	return new ResponseEntity<String>("Usuario deletado com sucesso", HttpStatus.OK);
+    	
+    }
 
     
 }
