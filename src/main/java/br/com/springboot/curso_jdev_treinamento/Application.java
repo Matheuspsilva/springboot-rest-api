6d7a804ec15d8e189f087fea4e7b7167b8413226
Matheus <matheuspsilva29@gmail.com>
2022-05-03 19:58:57 -0300

    Ajustes para deploy

--- src/main/java/br/com/springboot/curso_jdev_treinamento/Application.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/Application.java
@@ -2,13 +2,14 @@ package br.com.springboot.curso_jdev_treinamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  *
  * Spring Boot application starter class
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer  {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
